dfbdef3a5cef6b0014644ba508d22884d3eb8098
Kevin Escamilla <esc_kevin.11@outlook.com>
2019-01-17 12:43:26 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Kevin Escamilla
+ * Partner: Dillon Ketcham
+ * Date: Jan 17, 2019
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kevin Escamilla\n");
+
+  return 0;
+}
